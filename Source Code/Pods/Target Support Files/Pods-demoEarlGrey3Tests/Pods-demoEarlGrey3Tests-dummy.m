a9ad8f44217a4842cb258dd6bb7eecc9fceae069
Keval Ladumor <keval.ladumor@manektech.com>
2020-05-29 17:47:09 +0530

    no message

---
+++ Source Code/Pods/Target Support Files/Pods-demoEarlGrey3Tests/Pods-demoEarlGrey3Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_demoEarlGrey3Tests : NSObject
+@end
+@implementation PodsDummy_Pods_demoEarlGrey3Tests
+@end
---
+++ Source Code/demoEarlGrey3Tests/demoEarlGrey3Tests-Bridging-Header.h
@@ -0,0 +1,16 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+
+//#ifndef demoEarlGrey3Tests-Bridging-Header_h
+//#define demoEarlGrey3Tests-Bridging-Header_h
+//
+//#import "AppFramework/Action/GREYAction.h"
+//#import "AppFramework/Action/GREYActionBlock.h"
+//#import "AppFramework/Action/GREYActions.h"
+//#import "CommonLib/DistantObject/GREYHostApplicationDistantObject.h"
+//#import "CommonLib/Matcher/GREYElementMatcherBlock.h"
+//#import "CommonLib/Matcher/GREYMatcher.h"
+//#import "TestLib/AlertHandling/XCTestCase+GREYSystemAlertHandler.h"
+//#import "TestLib/EarlGreyImpl/EarlGrey.h"
